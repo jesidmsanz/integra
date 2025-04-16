8d432c156ef2b09418c657262a19f90aaba6a427
Jesid Moscote Sanchez <68261922+jesidmsanz@users.noreply.github.com>
2025-04-16 09:01:38 -0500

    fix

--- src/pages/admin/novedades/detalle/index.jsx
+++ src/pages/admin/novedades/detalle/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import RootLayout from "../../layout";
-import TypeNewsForm from "@/utils/Components/Admin/TypeNews/TypeNewsForm";
+import TypeNewsForm from "@/utils/Components/Admin/TypeNews/TypeNewsForm/TypeNewsForm";
 
 export default function Detalle() {
   return (
--- src/pages/admin/novedades_empleados/detalle/index.jsx
+++ src/pages/admin/novedades_empleados/detalle/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import RootLayout from "../../layout";
-import EmployeeNewsForm from "@/utils/Components/Admin/EmployeeNews/EmployeeNewsForm";
+import EmployeeNewsForm from "@/utils/Components/Admin/EmployeeNews/EmployeeNewsForm/EmployeeNewsForm";
 
 export default function Detalle() {
   return (
